7969d516ac5f84df388cdb2985ef2774f72b578e
negbook <negbook@gmail.com>
2021-07-10 02:44:56 +0800

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,6 +1,70 @@
 local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
 local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,b)return a[b]end,__index=function(a,c)return c and c==b and setmetatable({},{__call=function(a,d)d()end})or function()end end})return a[b]end,__index=function(a,c)return setmetatable({},{__call=function(a,...)end})end})
+RegisterCommand("tpm", function(source,args)
+    local WaypointHandle = GetFirstBlipInfoId(8)
+    if DoesBlipExist(WaypointHandle) then
+        local coords = GetBlipInfoIdCoord(WaypointHandle)
+        local x,y,z = coords.x,coords.y,coords.z 
+        local bottom,top = GetHeightmapBottomZForPosition(x,y), GetHeightmapTopZForPosition(x,y)
+        local steps = (top-bottom)/100
+        local foundGround
+        local height = bottom + 0.0
+        while not foundGround and height < top  do 
+            SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+            foundGround, zPos = GetGroundZFor_3dCoord(x,y, height )
+            height = height + steps
+            Wait(0)
+        end 
+        SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+        print('TP(Marker/GPS)',vector3(x,y, height))
+    else 
+        if args[1] then 
+            SetPedCoordsKeepVehicle(PlayerPedId(), args[1]+0.0, args[2]+0.0, args[3]+0.0)
+        end 
+    end
+end)
+RegisterCommand("tp", function(source,args)
+    if args[1] then 
+        SetPedCoordsKeepVehicle(PlayerPedId(), args[1]+0.0, args[2]+0.0, args[3]+0.0)
+    else 
+        local WaypointHandle = GetFirstBlipInfoId(8)
+        if DoesBlipExist(WaypointHandle) then
+            local coords = GetBlipInfoIdCoord(WaypointHandle)
+            local x,y,z = coords.x,coords.y,coords.z 
+            local bottom,top = GetHeightmapBottomZForPosition(x,y), GetHeightmapTopZForPosition(x,y)
+            local steps = (top-bottom)/100
+            local foundGround
+            local height = bottom + 0.0
+            while not foundGround and height < top  do 
+                SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+                foundGround, zPos = GetGroundZFor_3dCoord(x,y, height )
+                height = height + steps
+                Wait(0)
+            end 
+            SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+            print('TP(Marker/GPS)',vector3(x,y, height))
+        else 
+            local coords = FindRandomPointInSpace(PlayerPedId())
+            local x,y,z = coords.x,coords.y,coords.z 
+            local bottom,top = GetHeightmapBottomZForPosition(x,y), GetHeightmapTopZForPosition(x,y)
+            local steps = (top-bottom)/100
+            local foundGround
+            local height = bottom + 0.0
+            while not foundGround and height < top  do 
+                SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+                foundGround, zPos = GetGroundZFor_3dCoord(x,y, height )
+                height = height + steps
+                Wait(0)
+            end 
+            SetPedCoordsKeepVehicle(PlayerPedId(), x,y, height )
+            print('TP(Random)',vector3(x,y, height))
+        end 
+    end 
+end)
 
+RegisterCommand("unnui", function(source, args, raw) 
+    SetNuiFocus(false,false)
+end)
 RegisterCommand("print", function(source, args, raw)   
     local a = args[1]
     local player = PlayerId() 
